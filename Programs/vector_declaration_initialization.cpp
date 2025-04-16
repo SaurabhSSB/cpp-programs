f98ccb878dcd17320334794f81e9feb5c1247c41
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-04-16 23:01:40 +0530

    Adding vector_declaration_initialization program file

---
+++ Programs/vector_declaration_initialization.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+int main() {
+
+    // Declare empty vector
+    vector<int> vc;
+
+    // Declare and initialize with value
+    vector<int> vc2= {11, 12, 14, 16};
+
+    // Declare with a fixed size and default value
+    vector<int> vc3 (5,10); // Size is 5 and all elements are intialized by 10.
+
+    return 0;
+}
